e82e263c09e97ed8743fec037f5759283490ac62
Yugansh Marwah <yugansh@cldcvr.com>
2020-01-25 02:29:12 +0530

    testing PR trigger

--- app/src/controllers.js
+++ app/src/controllers.js
@@ -2,7 +2,7 @@ const config = require('./../config')
 
 function hello() { 
     return  { 
-        msg: 'Hello, Singapore!', 
+        msg: 'Hello, PR tesing!', 
         environment: config.ENV_NAME
     }
 };
