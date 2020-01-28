de87aabfabd94ad67da83026a3d3605ef5b4e5ee
yuganshmarwah <yuganshmarwah@gmail.com>
2020-01-28 16:02:45 +0530

    Test prop pipeline

--- app/src/controllers.js
+++ app/src/controllers.js
@@ -2,7 +2,7 @@ const config = require('./../config')
 
 function hello() { 
     return  { 
-        msg: 'Hello, first pipeline build 7!', 
+        msg: 'Hello, first pipeline build 10!', 
         environment: config.ENV_NAME
     }
 };
