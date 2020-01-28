2b7a3b57ae55744d6098801e925dcad4d1de91eb
Yugansh Marwah <yugansh@cldcvr.com>
2020-01-28 17:41:12 +0530

    Testing after proper integration

--- app/src/controllers.js
+++ app/src/controllers.js
@@ -2,7 +2,7 @@ const config = require('./../config')
 
 function hello() { 
     return  { 
-        msg: 'Hello, first pipeline build 10!', 
+        msg: 'Hello, first pipeline build 11!', 
         environment: config.ENV_NAME
     }
 };
