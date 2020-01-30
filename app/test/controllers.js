6bb9fe27fbc77a0c9fe7c9e7e58cc10745536701
yuganshmarwah <yuganshmarwah@gmail.com>
2020-01-30 17:25:25 +0530

    Test updated

--- app/test/controllers.js
+++ app/test/controllers.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 const controllers = require('../src/controllers')
 
-const HOME_API_EXPECTED_MSG = 'Hello, first pipeline build 15!';
+const HOME_API_EXPECTED_MSG = 'Hello, first pipeline build 16!';
 
 describe('Controllers', function() {
   describe('hello', function() {
