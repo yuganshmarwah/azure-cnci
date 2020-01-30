95529fbd8d489a093a87485677947e77fe6d8b74
yuganshmarwah <yuganshmarwah@gmail.com>
2020-01-30 17:19:31 +0530

    Testing point 1

--- app/src/controllers.js
+++ app/src/controllers.js
@@ -2,7 +2,7 @@ const config = require('./../config')
 
 function hello() { 
     return  { 
-        msg: 'Hello, first pipeline build 15!', 
+        msg: 'Hello, first pipeline build 16!', 
         environment: config.ENV_NAME
     }
 };
