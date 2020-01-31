e1b040f44d76a77b3d1168ae1f5bad7d4d289e84
yuganshmarwah <yuganshmarwah@gmail.com>
2020-01-31 18:22:53 +0530

    Update controllers.js

--- app/src/controllers.js
+++ app/src/controllers.js
@@ -2,7 +2,7 @@ const config = require('./../config')
 
 function hello() { 
     return  { 
-        msg: 'Hello, first pipeline build 17!', 
+        msg: 'Hello, first pipeline build 18!', 
         environment: config.ENV_NAME
     }
 };
